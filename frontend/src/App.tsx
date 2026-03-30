fb7a7ea978d931a444d5666f1a6291268fa9363d
lujia chen <lujiac1@uci.edu>
2026-03-30 11:39:05 -0400

    feat: add /algorithms and /custom routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './pages/Home'
 import Post from './pages/Post'
+import Algorithms from './pages/Algorithms'
+import CustomAnalysis from './pages/CustomAnalysis'
 import './index.css'
 
 export default function App() {
@@ -11,6 +13,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:slug" element={<Post />} />
+        <Route path="/algorithms" element={<Algorithms />} />
+        <Route path="/custom" element={<CustomAnalysis />} />
       </Routes>
     </BrowserRouter>
   )
